bb91d1af1c220d4266dad24d480972f9c211c8f1
dominikdobos <dominik.dobos33@gmail.com>
2025-02-24 10:08:18 +0100

    garázs elkezdése, constructor, setter, getter, ki és beáll metódusok

---
+++ src/main/java/hu/szamalk/modell/Garazs.java
@@ -0,0 +1,52 @@
+package hu.szamalk.modell;
+
+import java.util.ArrayList;
+
+public class Garazs {
+    private int ferohely;
+    private ArrayList<Jarmu> jarmuvek;
+    private static final int MAX_FEROHELY = 5;
+
+    public Garazs(ArrayList<Jarmu> jarmuvek) {
+        setJarmuvek(jarmuvek);
+        this.ferohely = 5 - jarmuvek.size();
+    }
+
+    public ArrayList<Jarmu> getJarmuvek() {
+        return new ArrayList<>(this.jarmuvek);
+    }
+
+    public void setJarmuvek(ArrayList<Jarmu> jarmuvek) {
+        if (jarmuvek.size() <= MAX_FEROHELY) {
+            this.jarmuvek = jarmuvek;
+        } else {
+            throw new RuntimeException("Nincs elég hely a garázsban.");
+        }
+    }
+
+    public void jarmuBeall(Jarmu jarmu) {
+        if (this.jarmuvek.size() < MAX_FEROHELY) {
+            this.jarmuvek.add(jarmu);
+            this.ferohely--;
+        } else {
+            throw new RuntimeException("Nincs elég hely a garázsban.");
+        }
+    }
+
+    public void jarmuKiall(Jarmu jarmu) {
+        if (this.jarmuvek.contains(jarmu)) {
+            this.jarmuvek.remove(jarmu);
+            this.ferohely++;
+        } else {
+            throw new RuntimeException("Nincs ilyen jármű a garázsban.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Garazs{" +
+                "ferohely=" + ferohely +
+                ", jarmuvek=" + jarmuvek +
+                '}';
+    }
+}
